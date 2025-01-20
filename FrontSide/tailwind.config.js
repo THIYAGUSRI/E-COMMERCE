a170bee8732d7bad7d944e8e8910846381721b80
@THIYAGUSRI <thiyagusrinivasan09@gmail.com>
2025-01-20 08:44:17 +0530

    update

--- FrontSide/tailwind.config.js
+++ FrontSide/tailwind.config.js
@@ -1,15 +1,13 @@
+const flowbite = require("flowbite-react/tailwind");
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}", // Correct Flowbite React paths
+    flowbite.content(),
   ],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('flowbite/plugin'), // Correct Flowbite plugin
-    require('tailwind-scrollbar'), // Your additional plugin
-  ],
-};
+  plugins: [flowbite.plugin(), require('tailwind-scrollbar'),],
+}
